af51ee161c3c7de973ad835ae7f61f19b28ec262
Remy Vanherweghem <remy.vanherweghem@parl.gc.ca>
2025-10-31 13:25:33 +0000

    Temporary icon placeholder.

--- src/App.ce.vue
+++ src/App.ce.vue
@@ -1,14 +1,12 @@
 <template>
   <DebugBar v-if="debug"></DebugBar>
   <div class="flex flex-col justify-center items-center gap-8">
-    <img alt="PBO-DBP" :src="logoUrlComputed" class="w-64" />
     <ToolSplash />
   </div>
 </template>
 
 <script setup>
 import { defineAsyncComponent, computed, onMounted, watch, getCurrentInstance } from 'vue'
-import logoUrl from "./assets/logo.svg?url";
 import ToolSplash from './components/ToolSplash.vue'
 import WrapperEventDispatcher from "./WrapperEventDispatcher.js"
 import { storeToRefs } from 'pinia'
@@ -23,7 +21,6 @@ const instance = getCurrentInstance()
 const localizationsStore = useLocalizationsStore()
 const { language, strings } = storeToRefs(localizationsStore)
 
-const logoUrlComputed = computed(() => logoUrl)
 const debug = computed(() => instance.proxy.$root.debug)
 
 const setPageTitle = () => {
