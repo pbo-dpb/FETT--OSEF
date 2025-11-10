5dc0a08a648474999ffa84040c585e58a5d694e4
Remy Vanherweghem <remy.vanherweghem@parl.gc.ca>
2025-11-10 16:01:03 +0000

    Added department picker..

---
+++ src/components/DepartmentsOverview/DepartmentPicker.vue
@@ -0,0 +1,93 @@
+<template>
+    <ScrollAreaRoot class=" h-96 relative  overflow-hidden border border-solid rounded-lg"
+        style="--scrollbar-size: 10px">
+        <div class="absolute top-0 z-10 w-full h-6 bg-gradient-to-t from-transparent to-white" />
+        <ScrollAreaViewport class="w-full h-full rounded">
+            <div class="p-4">
+                <div><input type="search" v-model="query"
+                        class="w-full mb-4 px-2 py-1 border border-slate-300 border-solid rounded"
+                        :placeholder="strings.search_departments_placeholder" /></div>
+                <RouterLink :to="department.route" v-for="department in sortedDepartments" :key="department.id"
+                    class="flex flex-row items-center justify-between text-xs mt-2 pt-2 first:border-t-0 border-t border-slate-100 border-solid cursor-pointer hover:text-sky-700 group">
+                    <span>{{ department.name }}</span>
+                    <Plus v-if="!department.active"
+                        class="size-4 shrink-0 text-sky-700 opacity-0 group-hover:opacity-100"></Plus>
+                    <CircleX v-if="department.active" class="size-4 shrink-0 text-red-700 hidden group-hover:block">
+                    </CircleX>
+                    <Check v-if="department.active" class="size-4 shrink-0 text-green-700 block group-hover:hidden">
+                    </Check>
+                </RouterLink>
+            </div>
+        </ScrollAreaViewport>
+        <ScrollAreaScrollbar
+            class="select-none touch-none p-0.5 z-20 bg-white transition-colors duration-150 ease-out hover:bg-slate-100 w-3 "
+            orientation="vertical">
+            <ScrollAreaThumb class="flex-1 bg-slate-500 rounded-sm" />
+        </ScrollAreaScrollbar>
+
+
+        <div class="absolute bottom-0 z-10 w-full h-6 bg-gradient-to-b from-transparent to-white" />
+    </ScrollAreaRoot>
+</template>
+<script setup>
+import { Check, CircleX, Plus } from 'lucide-vue-next';
+
+import Fuse from 'fuse.js'
+import { ScrollAreaRoot, ScrollAreaScrollbar, ScrollAreaThumb, ScrollAreaViewport } from 'reka-ui'
+
+import { storeToRefs } from 'pinia'
+import usePayloadsStore from '../../stores/payloads.js'
+import useLocalizationsStore from '../../stores/localizations.js'
+import { ref, computed } from 'vue'
+const payloadsStore = usePayloadsStore()
+const { departments } = storeToRefs(payloadsStore)
+const localizationStore = useLocalizationsStore()
+const { language, strings } = storeToRefs(localizationStore)
+import { useRoute } from 'vue-router'
+const route = useRoute()
+
+const query = ref('')
+
+const currentlySelectedDepartments = computed(() => {
+    return route.params.departments || []
+})
+
+const deptToListableObject = (dept) => {
+
+    const isCurrentlySelected = currentlySelectedDepartments.value.includes(dept.id);
+
+    let route;
+    if (isCurrentlySelected) {
+        route = { name: 'departments', params: { departments: currentlySelectedDepartments.value.filter(id => id !== dept.id) } }
+    } else {
+        const newSelectedDepartments = [...currentlySelectedDepartments.value, dept.id]
+        route = { name: 'departments', params: { departments: newSelectedDepartments } }
+    }
+
+    return {
+        'name': dept[`name_${language.value}`],
+        'acronym': dept[`acronym_${language.value}`],
+        'id': dept.id,
+        'route': route,
+        'active': isCurrentlySelected
+    }
+}
+
+const sortedDepartments = computed(() => {
+    if (departments.value === false) {
+        return []
+    }
+
+
+    if (query.value.trim()) {
+        const fuse = new Fuse(departments.value.map(dept => deptToListableObject(dept)), {
+            keys: ['name', 'acronym'],
+        })
+
+        return fuse.search(query.value).map(result => result.item)
+    }
+
+    return [...departments.value].sort((a, b) => a[`name_${language.value}`].localeCompare(b[`name_${language.value}`])).map(dept => deptToListableObject(dept))
+})
+
+</script>
--- src/components/DepartmentsOverview/DepartmentsOverview.vue
+++ src/components/DepartmentsOverview/DepartmentsOverview.vue
@@ -1,9 +1,38 @@
-<script setup>
-
-</script>
-
 <template>
 
-    <div>Overview by Department</div>
+    <div class="flex flex-col gap-4" v-if="departments !== false">
+        <div>OVERVIEW</div>
+
+        <div class="grid grid-cols-4 gap-4">
+
+            <DepartmentPicker />
+            <div class="col-span-3">
+                PICKED DEPARTMENTS
+            </div>
+
+
+        </div>
+
+    </div>
+    <div v-else>
+        <LoadingIndicator class="size-6" />
+    </div>
 
 </template>
+<script setup>
+import { storeToRefs } from 'pinia'
+import { onMounted } from 'vue'
+import usePayloadsStore from '../../stores/payloads.js'
+const payloadsStore = usePayloadsStore()
+const { departments } = storeToRefs(payloadsStore)
+import DepartmentPicker from './DepartmentPicker.vue'
+import LoadingIndicator from '../LoadingIndicator.vue'
+
+onMounted(() => {
+    if (departments.value === false) {
+        payloadsStore.fetchDepartments();
+    }
+});
+
+
+</script>
