b4757f0a7d24b759fa26efe316e217bf4c929f04
Remy Vanherweghem <remy.vanherweghem@parl.gc.ca>
2025-11-10 12:41:54 +0000

    Lazy loading of routes' components.

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import { createWebHashHistory, createRouter } from 'vue-router'
 
-import PublicServiceOverview from './components/PublicServiceOverview/PublicServiceOverview.vue'
-import DepartmentsOverview from './components/DepartmentsOverview/DepartmentsOverview.vue'
+const PublicServiceOverview = () => import('./components/PublicServiceOverview/PublicServiceOverview.vue')
+const DepartmentsOverview = () => import('./components/DepartmentsOverview/DepartmentsOverview.vue')
 
 
 const routes = [
