4674f9ab69aaa73902ac2ca1670afc4495a41c96
Remy Vanherweghem <remy.vanherweghem@parl.gc.ca>
2025-12-12 20:53:54 +0000

    Present values for RCPM members and CAF differently.

--- src/components/DepartmentsOverview/PickedDepartment.vue
+++ src/components/DepartmentsOverview/PickedDepartment.vue
@@ -35,15 +35,25 @@
         </header>
         <div class="border-t-2 border-slate-100 border-solid px-4 pt-4 ">
 
-
-            <dl class="flex flex-col gap-1">
+            <div v-if="department.latest_ftes.indeterminate === 0 && department.latest_ftes.combined > 0">
+                <dl class="flex flex-col gap-1">
+                    <template v-for="(value, key) in { combined: department.latest_ftes.combined }" :key="key">
+                        <dt class="font-medium text-xs">{{ strings[`dep_card_${key}`] }}</dt>
+                        <dd class="text-lg font-light">{{ numberFormatter(Math.round(value)) }}</dd>
+                    </template>
+                </dl>
+                <div class="text-gray-500 text-sm italic text-base/8">{{ strings.department_latest_ftes_combined_note
+                    }}</div>
+            </div>
+            <dl class="flex flex-col gap-1" v-else>
                 <template
-                    v-for="(value, key) in { indeterminate: department.latest_ftes.indeterminate, term: department.latest_ftes.term, casual: department.latest_ftes.casual, student: department.latest_ftes.student, combined: department.latest_ftes.combined }"
+                    v-for="(value, key) in { indeterminate: department.latest_ftes.indeterminate, term: department.latest_ftes.term, casual: department.latest_ftes.casual, student: department.latest_ftes.student }"
                     :key="key">
                     <dt class="font-medium text-xs">{{ strings[`dep_card_${key}`] }}</dt>
                     <dd class="text-lg font-light">{{ numberFormatter(Math.round(value)) }}</dd>
                 </template>
             </dl>
+
 
             <div class="w-full text-xs font-medium text-slate-500 text-right mt-4">
                 {{ strings.department_latest_ftes_as_of.replace('{date}', new Intl.DateTimeFormat(language, {
@@ -63,6 +73,7 @@ import { CircleX } from 'lucide-vue-next';
 import { storeToRefs } from 'pinia'
 import usePayloadsStore from '../../stores/payloads.js'
 import useLocalizationsStore from '../../stores/localizations.js'
+import { onMounted } from 'vue';
 
 const emits = defineEmits(['remove-department', 'highlight-department', 'unhighlight-department']);
 
@@ -84,5 +95,6 @@ const props = defineProps({
 const numberFormatter = (number) => {
     return localizationStore.localizeNumber(number);
 }
+
 
 </script>
